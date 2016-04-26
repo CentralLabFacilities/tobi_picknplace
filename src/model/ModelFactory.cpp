2dcc19245bd822d5b284d08a67b57be0a6179896
Nils Neumann <nneumann@techfak.uni-bielefeld.de>
2016-04-26 15:14:36 +0200

    test without diff

--- src/model/ModelFactory.cpp
+++ src/model/ModelFactory.cpp
@@ -8,21 +8,21 @@
 #include "ModelFactory.h"
 
 #include "types/H2R5.h"
-#ifdef USE_KATANA
+//#ifdef USE_KATANA
 #include "types/Katana.h"
 #include "types/KatanaSim.h"
-#endif //USE_KATANA
+//#endif //USE_KATANA
 
 Model::Ptr ModelFactory::create(std::string const &type) {
 
   std::cout << "Factory" << std::endl;
-#ifdef USE_KATANA
+//#ifdef USE_KATANA
     if(type == KATANA_NAME)
         std::cout << "create Katana" << std::endl;
         return Katana::Ptr(new Katana());
     if(type == KATANA_SIM_NAME)
         return KatanaSim::Ptr(new KatanaSim());
-#endif //USE_KATANA
+//#endif //USE_KATANA
     if(type == H2R5_NAME)
         return H2R5::Ptr(new H2R5());
     std::cout << "Found error" << std::endl;
