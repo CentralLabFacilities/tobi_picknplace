d69382c72b8e7456afafd5097bb24f889bd1f740
Nils Neumann <nneumann@techfak.uni-bielefeld.de>
2016-04-26 16:21:30 +0200

    next test

--- src/model/ModelFactory.cpp
+++ src/model/ModelFactory.cpp
@@ -17,15 +17,15 @@ Model::Ptr ModelFactory::create(std::string const &type) {
 
   std::cout << "Factory" << std::endl;
 //#ifdef USE_KATANA
-    if(type == KATANA_NAME)
+    if(type == KATANA_NAME){
 	Katana::Ptr katana = Katana::Ptr(new Katana());
 	std::cout << "create Katana" << std::endl;
-        return katana;
+        return katana;}
     //if(type == KATANA_SIM_NAME)
    //     return KatanaSim::Ptr(new KatanaSim());
 //#endif //USE_KATANA
-    if(type == H2R5_NAME)
-        return H2R5::Ptr(new H2R5());
+    if(type == H2R5_NAME){
+        return H2R5::Ptr(new H2R5());}
     std::cout << "Found error" << std::endl;
     return Model::Ptr();
 
