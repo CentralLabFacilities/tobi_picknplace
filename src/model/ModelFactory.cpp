115e2ac55bfaef1c283732eb4e6ef7038f4a6c65
Nils Neumann <nneumann@techfak.uni-bielefeld.de>
2016-04-27 14:59:53 +0200

    add USE_KATANA

--- src/model/ModelFactory.cpp
+++ src/model/ModelFactory.cpp
@@ -8,21 +8,21 @@
 #include "ModelFactory.h"
 
 #include "types/H2R5.h"
-//#ifdef USE_KATANA
+#ifdef USE_KATANA
 #include "types/Katana.h"
 #include "types/KatanaSim.h"
-//#endif //USE_KATANA
+#endif //USE_KATANA
 
 Model::Ptr ModelFactory::create(std::string const &type) {
 
-//#ifdef USE_KATANA
+#ifdef USE_KATANA
     if(type == KATANA_NAME){
 	Model::Ptr katana;
 	katana = Model::Ptr(new Katana());
         return katana;}
     //if(type == KATANA_SIM_NAME)
     //    return KatanaSim::Ptr(new KatanaSim());
-//#endif //USE_KATANA
+#endif //USE_KATANA
     if(type == H2R5_NAME){
         return H2R5::Ptr(new H2R5());}
     std::cout << "Load default Model" << std::endl;
