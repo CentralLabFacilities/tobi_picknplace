4f5e037412ef68f55fcd8d05b6c6ee5cbfacf6b1
Nils Neumann <nneumann@techfak.uni-bielefeld.de>
2016-05-10 16:35:53 +0200

    cast

--- src/interface/AGNIInterface.cpp
+++ src/interface/AGNIInterface.cpp
@@ -108,7 +108,8 @@ void AGNIInterface::generateAllGrasps(){
       vector<moveit_msgs::Grasp> grasp = generate_grasps(obj);
       i = i + grasp.size();
        for(moveit_msgs::Grasp graspv2: grasp) {
-	 graspv2.id = boost::lexical_cast<string>(boost::lexical_cast<int>(graspv2.id) + i);
+	 int id = boost::lexical_cast<int>(graspv2.id) + i;
+	 graspv2.id = std::to_string(id);
 	 ROS_ERROR_STREAM("Push Grasp back " + graspv2.id);
 	 graps.push_back(graspv2);
        }
