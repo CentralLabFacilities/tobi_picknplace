a1b203febf4a40ea09b2d9fbfa01c51a407b3632
Nils Neumann <nneumann@techfak.uni-bielefeld.de>
2016-05-10 14:46:41 +0200

    add debug msg

--- src/interface/AGNIInterface.cpp
+++ src/interface/AGNIInterface.cpp
@@ -98,6 +98,8 @@ vector<grasping_msgs::Object> AGNIInterface::find_objects(bool plan_grasps = fal
 }
 
 void AGNIInterface::generateAllGrasps(){
+  
+    ROS_ERROR_STREAM("Generate all Grasps for Debugging");
     grasping_msgs::FindGraspableObjectsGoal goal;
     goal.plan_grasps = true;
     vector<moveit_msgs::Grasp> grasp;
@@ -106,6 +108,7 @@ void AGNIInterface::generateAllGrasps(){
     grasping_msgs::FindGraspableObjectsResult::ConstPtr results = cl_object_fitter->getResult();
     
     for(grasping_msgs::GraspableObject obj: results->objects) {
+      ROS_ERROR_STREAM("Object: _" + obj.object.name);
       grasp = generate_grasps(obj.object);
       rosTools.display_grasps(grasp);
     } 
