2d4bd32058b96ce9d57153ffc8af88d371718171
Nils Neumann <nneumann@techfak.uni-bielefeld.de>
2016-05-20 15:41:10 +0200

    turn orientation on 180°

--- src/model/types/Katana.cpp
+++ src/model/types/Katana.cpp
@@ -296,7 +296,7 @@ GraspReturnType Katana::graspObject(const string &obj, const string &surface,
     for(moveit_msgs::Grasp &i : grasps)
     {
       Eigen::Quaternionf quat(i.grasp_pose.pose.orientation.w,i.grasp_pose.pose.orientation.x,i.grasp_pose.pose.orientation.y,i.grasp_pose.pose.orientation.z);// = i.grasp_pose.pose.orientation; 
-      Eigen::Quaternionf rotation(Eigen::AngleAxisf(0.5*M_PI, Eigen::Vector3f::UnitY())); // AngleAxis<float>(angle_in_radian, axis);
+      Eigen::Quaternionf rotation(Eigen::AngleAxisf(-0.5*M_PI, Eigen::Vector3f::UnitY())); // AngleAxis<float>(angle_in_radian, axis);
       //rotation.w = 0.7071;
       //rotation.x = 0.0;
       //rotation.y = 0.7071;
