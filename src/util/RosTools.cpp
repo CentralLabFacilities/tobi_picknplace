e9fd5fc9ff058a75c8273142792bafe73204ccda
Nils Neumann <nneumann@techfak.uni-bielefeld.de>
2016-05-31 17:12:37 +0200

    Print the object we publish as collision object

--- src/util/RosTools.cpp
+++ src/util/RosTools.cpp
@@ -82,6 +82,8 @@ GraspReturnType::GraspResult RosTools::graspResultFromMoveit(
 
 void RosTools::publish_collision_object(grasping_msgs::Object msg) {
 
+    
+    ROS_DEBUG_STREAM("Publish object with name " << msg.name);
     ParamReader& params = ParamReader::getParamReader();
 
     moveit_msgs::CollisionObject target_object;
